39fb66ca0f5f6cce459c7dd0ef0676989e366afb
Christopher Dingli <cldingli@gmail.com>
2014-01-18 17:01:52 +1000

    create rubygem from original pixelholder project

---
+++ pixelholder.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'pixelholder'
+  s.version     = '1.2.1'
+  s.date        = '2014-01-18'
+  s.summary     = "A placeholder image generator"
+  s.description = "A simple placeholder image generator"
+  s.authors     = ["Christopher Dingli"]
+  s.email       = 'cldingli@gmail.com'
+  s.files       = ["lib/pixelholder.rb"]
+  s.homepage    = 'http://github.com/chrisdingli/pixelholder-rubygem'
+  s.license     = 'MIT'
+end
---
+++ test/test_pixelholder.rb
@@ -0,0 +1,49 @@
+require 'test/unit'
+require 'pixelholder'
+
+class PixelHolderTest < Test::Unit::TestCase
+
+  def setup
+    options = Hash.new(false)
+    @pixelholder = PixelHolder.new('color:000', '300', options)
+  end
+
+  def teardown
+    #
+  end
+
+  def test_generate_color
+    assert_equal "ff0000",
+      @pixelholder.generate_color("f00")
+    assert_equal "ff0000",  
+      @pixelholder.generate_color("ff00")
+    assert_equal "ff0000",    
+      @pixelholder.generate_color("ff000")
+    assert_equal "ff0000",  
+      @pixelholder.generate_color("ff00000")
+    assert_equal "f0f0f0",
+      @pixelholder.generate_color("f0")
+    assert_equal "ffffff",
+      @pixelholder.generate_color("f")
+    assert_equal "FFFFFF",
+      @pixelholder.generate_color("F")
+  end 
+
+  def test_get_hex
+    assert_equal '#d3d3d3', 
+      @pixelholder.get_hex('d3d3d3')
+    assert_equal '#f3f3f3', 
+      @pixelholder.get_hex('f3f3f3987456')
+    assert_equal '#ffff00', 
+      @pixelholder.get_hex('ffff')
+    assert_equal '#ff33dd', 
+      @pixelholder.get_hex('f3d')
+    assert_equal '#f3f3f3', 
+      @pixelholder.get_hex('f3')
+    assert_equal '#ffffff', 
+      @pixelholder.get_hex('f')
+    assert_equal '#ffffff', 
+      @pixelholder.get_hex('F')
+  end
+
+end
